9e25de9262ac484531091ea049b3fe58b598084a
yunshu-cloud <w2099266050@gmail.com>
2023-08-28 02:18:15 +0800

    security配置

--- shopping_manager_api/src/main/java/com/yunshucloud/shopping_manager_api/security/MyLoginFailureHandler.java
+++ shopping_manager_api/src/main/java/com/yunshucloud/shopping_manager_api/security/MyLoginFailureHandler.java
@@ -3,16 +3,19 @@ package com.yunshucloud.shopping_manager_api.security;
 import com.alibaba.fastjson.JSON;
 import com.yunshucloud.shopping_common.result.BaseResult;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-// 登录成功处理器
-public class MyLoginFailureHandler implements AuthenticationSuccessHandler {
+// 登录失败处理器
+public class MyLoginFailureHandler implements AuthenticationFailureHandler {
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException{
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setContentType("text/json;charset=utf-8");
         BaseResult result = new BaseResult(402, "用户名或密码错误", null);
         response.getWriter().write(JSON.toJSONString(result));

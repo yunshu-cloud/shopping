df6db9f8b751e7b0e7eaa511e1bd5838774a3e41
yunshu-cloud <w2099266050@gmail.com>
2023-08-28 02:11:59 +0800

    security处理器

---
+++ shopping_manager_api/src/main/java/com/yunshucloud/shopping_manager_api/security/MyAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.yunshucloud.shopping_manager_api.security;
+
+import com.alibaba.fastjson.JSON;
+import com.yunshucloud.shopping_common.result.BaseResult;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 权限不足处理器
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("text/json;charset=utf-8");
+        BaseResult result = new BaseResult(403, "权限不足", null);
+        response.getWriter().write(JSON.toJSONString(result));
+    }
+}
+
---
+++ shopping_manager_api/src/main/java/com/yunshucloud/shopping_manager_api/security/MyLoginSuccessHandler.java
@@ -0,0 +1,23 @@
+package com.yunshucloud.shopping_manager_api.security;
+
+import com.alibaba.fastjson.JSON;
+import com.yunshucloud.shopping_common.result.BaseResult;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 登录成功处理器
+public class MyLoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException{
+        response.setContentType("text/json;charset=utf-8");
+        BaseResult result = new BaseResult(200, "登录成功", null);
+        response.getWriter().write(JSON.toJSONString(result));
+    }
+}
+
+
+

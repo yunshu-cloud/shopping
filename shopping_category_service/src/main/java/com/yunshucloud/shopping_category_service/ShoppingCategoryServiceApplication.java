8046402c39d33ee8e9f3b52d0bfc6ced2b73788a
yunshu-cloud <w2099266050@gmail.com>
2023-08-31 13:22:36 +0800

    广告用户api模块

---
+++ shopping_category_service/src/main/java/com/yunshucloud/shopping_category_service/ShoppingCategoryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.yunshucloud.shopping_category_service;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.yunshucloud.shopping_category_service.mapper")
+public class ShoppingCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCategoryServiceApplication.class, args);
+	}
+
+
+}

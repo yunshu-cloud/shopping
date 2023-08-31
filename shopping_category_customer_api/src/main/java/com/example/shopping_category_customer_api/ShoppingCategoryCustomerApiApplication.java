3e7f866e334541bf1c0c5da81f858fd2a76ec59f
yunshu-cloud <w2099266050@gmail.com>
2023-08-31 13:07:23 +0800

    广告用户api模块

---
+++ shopping_category_customer_api/src/main/java/com/example/shopping_category_customer_api/ShoppingCategoryCustomerApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.shopping_category_customer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class ShoppingCategoryCustomerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCategoryCustomerApiApplication.class, args);
+    }
+
+}
